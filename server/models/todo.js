61f1467e06615353bdd46b54f46a32b5efc97828
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-03-17 14:28:42 +0100

    commitx

---
+++ server/models/todo.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+    const Todo = sequelize.define('Todo', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+
+
+    return Todo;
+};
