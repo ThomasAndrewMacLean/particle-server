98a2504009160d6d5d372de0676383f4d1cc7763
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-03-19 15:24:09 +0100

    test

--- server/config/config.js
+++ server/config/config.js
@@ -6,18 +6,18 @@ module.exports = {
         'host': 'horton.elephantsql.com',
         'dialect': 'postgres'
     },
-    // 'test': {
-    //     'username': 'atizqvjs',
-    //     'password': 'hH-E0vBodx3_9YDHldqn3tMVHThNiNYz',
-    //     'database': 'atizqvjs',
-    //     'host': 'horton.elephantsql.com',
-    //     'dialect': 'postgres'
-    // },
-    // 'production': {
-    //     'username': 'atizqvjs',
-    //     'password': 'hH-E0vBodx3_9YDHldqn3tMVHThNiNYz',
-    //     'database': 'atizqvjs',
-    //     'host': 'horton.elephantsql.com',
-    //     'dialect': 'postgres'
-    // }
+    'test': {
+        'username': 'atizqvjs',
+        'password': process.env.DB_PW,
+        'database': 'atizqvjs',
+        'host': 'horton.elephantsql.com',
+        'dialect': 'postgres'
+    },
+    'production': {
+        'username': 'atizqvjs',
+        'password': process.env.DB_PW,
+        'database': 'atizqvjs',
+        'host': 'horton.elephantsql.com',
+        'dialect': 'postgres'
+    }
 };
